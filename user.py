64d79314a18209d6c22e007ef4e069971d16a7b1
Eren Demirci <merendmrcc@gmail.com>
2024-04-13 16:07:31 +0300

    remove_house function has been added to the User class.

--- user.py
+++ user.py
@@ -3,7 +3,17 @@ from house import House
 
 
 class User():
-
+    """
+    Represents a user in the system.
+    This class stores user information such as name, surname, email, password (hashed),
+    and a list of houses associated with the user.
+    Methods:
+        new_house(): Adds a house to the user's list.
+        remove_house(): Removes a house from the user's list.
+        change_password(): Changes the user's password.
+        get_house_names(): Returns a comma-separated string of house names.
+        show_info(): Displays the user's information in a formatted string.
+    """
     def __init__(self,name,surname,email,password) -> None:
         self.name = name
         self.surname = surname
@@ -13,10 +23,13 @@ class User():
 
     def new_house(self,house):
         """
-        The function adds a new "House" object to the user's list of houses.
-        It takes a "House" object as input
+        Adds a new "House" object to the user's list of houses.
+        Args:
+            house (House): The house object to be added.
+        Raises:
+            TypeError: If the argument is not a House object.
+            ValueError: If the house already exist in the user's house list.
         """
-        #Check if parameters is "House" object
         try:
             if isinstance(house, House):
                 if (self.houses.__contains__(house)):
@@ -25,19 +38,49 @@ class User():
                     self.houses.append(house)
                     print("The new house have been succsefully added.")
             else:
-                error_msg = f"The input 'house' must be an object from 'House' class. Given: {type(house)}"
+                error_msg = f"The input 'house' must be an object from 'House' class. Received: {type(house)}"
                 raise TypeError(error_msg)
         except TypeError as e:
             print(e)
         except OverflowError as e:
             print(e)
 
-    def change_password(self,password,new_password,new_password2):
+    
+    def remove_house(self,house):
+        """
+        Removes a house from the user's houses list.
+        Args:
+            house (House): The House object to be removed.
+        Raises:
+            TypeError: If the argument is not a House object.
+            ValueError: If the house does not exist in the user's house list.
+        """
+        try:
+            if not(isinstance(house,House)):
+                raise TypeError(f"The argument must be a 'House' object. Received: {type(house)}")
+            elif not(self.houses.__contains__(house)):
+                raise ValueError("House does not exist in the user houses list.")
+            self.houses.remove(house)
+            print("The house has been succsesfully removed in user houses list.")
+    
+        except TypeError as e :
+            print(e)
+        except ValueError as e : 
+            print(e)
 
+
+    def change_password(self,password,new_password,new_password2):
         """
-        The function changes user password.
-        Takes 3 parameters as string: password, new_password, new_password2
+        Changes the user's password.
+        Args:
+            password (str): The user's current password.
+            new_password (str): The new password the user wants to set.
+            new_password2 (str): A confirmation of the new password to ensure it's typed correctly.
+        Raises:
+            TypeError: If any of the parameters are not strings.
+            ValueError: If the current password is incorrect or the new passwords don't match.
         """
+        
         try:
             #Check if parameters is string
             if not isinstance(password, str) or not isinstance(new_password, str) or not isinstance(new_password2, str):
@@ -60,11 +103,10 @@ class User():
             print(e)
 
 
-    def house_names(self):
+    def get_house_names(self):
         """
         Returns the names of all houses as a comma-separated string.
-        *This function is specifically designed to work with the houses attribute of a User class.
-        
+        *This function is specifically designed to work with the houses attribute of a User class.  
         Returns:
             A comma-separated string containing the names of all houses.
         """
@@ -76,14 +118,13 @@ class User():
     def show_info(self):
         """
         Returns a formatted string containing the user's name, surname, email address, and a list of house names.
-
-        This function retrieves information from the `User` object and utilizes the `house_names` function to get the comma-separated house names.
-
+        This function retrieves information from the `User` object and utilizes the `get_house_names` function to get the comma-separated house names.
         Returns:
             A string with the user's details.
         """
         print(f"""
                 Name: {self.name} {self.surname}
                 E-mail: {self.email}
-                Houses: {self.house_names()}
+                Houses: {self.get_house_names()}
                 """)
+
